b4a8c24df0fef078a5bc125fd4c41c5776165205
kenhro <48067298+kenhro@users.noreply.github.com>
2020-03-25 10:38:49 -0400

    Update check_dac_output.ino
    Incorporated calibration to get us into range for coil driver voltage.

--- sketches/check_dac_output/check_dac_output.ino
+++ sketches/check_dac_output/check_dac_output.ino
@@ -1,31 +1,37 @@
 /*
   check_dac_output
 
-  Over a 30s span, ramp up DAC analog output level from 0 to 1023 so we can measure output levels.
+  Test calibration values; slowly step up DAC analog output level up to about 2V for 2.8 scale factor
+  that gets us to desired coil voltage.
 
-  modified 23 March 2020
+  modified 25 March 2020
   by Ken Hrovat
 */
 
+// define output pins
+#define out 15
+
 // constants won't change
 const long interval =  9000;  // milliseconds between amplitude steps
-const int dacStart = 0;       // atart DAC level output at this amount // 0, 725, 0
-const int dacStep = 1;       // step DAC level output by this amount with each iteration of main loop // 64, 1, 1
-const int dacEnd = 10;      // ending DAC level output at this amount // 1024, 740, 10
+const int dacStart = 435;     // atart DAC level output at this amount // 435
+const int dacStep = 25;       // step DAC level output by this amount with each iteration of main loop // 25
+const int dacEnd = 660;       // ending DAC level output at this amount // 660
+
+// Note: roughly-speaking, each change in DAC level output, gives about 3mV change in analog output voltage
 
 // variables will change
-int dacLevel = 0;
+int dacLevel = dacStart;
 
 void setup() {
   
   // initialize output pin
-  pinMode(15, OUTPUT);  // analog square wave output
+  pinMode(out, OUTPUT);  // analog square wave output
   
-  // change the analog write resolution to 10-bit
-  analogWriteResolution(10);  // default is 8-bit
+  // change the analog write resolution to 10-bit (1024 levels)
+  analogWriteResolution(10);  // default is 8-bit (256 levels)
 
   // initialize serial output
-  Serial.begin(9600); // baud rate must match what you use in Serial Monitor utility
+  Serial.begin(9600); // baud rate must match what we use in Serial Monitor utility
   while (!Serial);  // FIXME this blocks further execution until Serial Monitor is open/available
   Serial.println("setup done");
   Serial.println("========================================================================");
@@ -34,18 +40,15 @@ void setup() {
 
 void loop() {
 
-  for (int i = dacStart; i <= dacEnd; i+=dacStep) {
-
-    // get usable DAC level
-    dacLevel = min(i, 1023);
+  for (int dacLevel = dacStart; dacLevel <= dacEnd; dacLevel += dacStep) {
     
     // print DAC level
     Serial.println(dacLevel);
 
     // set output level
-    analogWrite(15, dacLevel);
+    analogWrite(out, dacLevel);
 
-    delay(interval);
+    delay(interval); // time to jot down output value in Excel and be ready for change to next
     
   }
   
